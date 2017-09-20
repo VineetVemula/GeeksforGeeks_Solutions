6f79dea4d368b755c30a3d8ec35a8992805caae4
Vineet Vemula <vindare@gmail.com>
2017-09-20 14:22:25 +0530

    Update insertinmiddle.c

--- LinkedList/insertinmiddle.c
+++ LinkedList/insertinmiddle.c
@@ -1,3 +1,31 @@
+Given a linked list of size N and a key, your task is to complete the function insertInMiddle(), that should insert the key in the middle of the linked list.
+
+Input:
+The function takes two argument as input, the reference pointer to the head of the linked list and an integer value key, the data to be inserted in the linked list.
+There will be T, test cases and for each test case the function will be called separately.
+
+Output:
+For each test case the function should return the reference pointer to the head of the linked list.
+
+Constraints:
+1<=T<=100
+1<=N<=103
+
+Example:
+Input:
+2
+3
+1 2 4
+3
+4
+10 20 40 50
+30
+Output:
+1 2 3 4
+10 20 30 40 50
+
+--CODE--
+
 Node* insertInMiddle(Node* head, int x)
 {
   int c=0;
