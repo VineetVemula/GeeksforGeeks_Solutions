5d705ecb216bd3f3f1ac3e0fc40b4e72d7710e5d
Vineet Vemula <vindare@gmail.com>
2017-10-19 19:27:34 +0530

    Create ops on sorted array.c

---
+++ Arrays/ops on sorted array.c
@@ -0,0 +1,45 @@
+Given a sorted array A[], your task is to do some operations, i.e., search an element, insert an element, and delete an element by completing the functions.
+
+Input:
+First line of input consists if T, number of test cases. First line of every test case consists of an integer N, denotes the number of elements in an array. Second line of every test cases consists of N spaced integers Ai. Third line of every test case consists of three integers X ,Y and Z, denoting the elements for search operation, insert operation and delete operation respectively.
+
+Output:
+Return 1 for each operation if done successfully else 0.
+
+Constraints:
+1<=T<=100
+1<=N<=100
+1<=Ai<=100
+
+Example:
+Input:
+1
+5
+1 2 3 4 5
+1 6 3
+Output:
+1 1 1
+
+--CODE--
+
+Given a sorted array A[], your task is to do some operations, i.e., search an element, insert an element, and delete an element by completing the functions.
+
+Input:
+First line of input consists if T, number of test cases. First line of every test case consists of an integer N, denotes the number of elements in an array. Second line of every test cases consists of N spaced integers Ai. Third line of every test case consists of three integers X ,Y and Z, denoting the elements for search operation, insert operation and delete operation respectively.
+
+Output:
+Return 1 for each operation if done successfully else 0.
+
+Constraints:
+1<=T<=100
+1<=N<=100
+1<=Ai<=100
+
+Example:
+Input:
+1
+5
+1 2 3 4 5
+1 6 3
+Output:
+1 1 1   
