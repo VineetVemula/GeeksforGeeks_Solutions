24e3fe6a4145793166c3d33f06b62d9a30665ec1
Vineet Vemula <vindare@gmail.com>
2017-10-24 19:19:04 +0530

    Create sum tree.c

---
+++ Trees/sum tree.c
@@ -0,0 +1,63 @@
+Write a function that returns true if the given Binary Tree is SumTree else false. A SumTree is a Binary Tree where value of every node x is equal to sum of nodes present in its left subtree and right subtree of x. An empty tree is SumTree and sum of an empty tree can be considered as 0. A leaf node is also considered as SumTree.
+
+Following is an example of SumTree.
+
+          26
+        /    \
+      10      3
+    /   \   /   \ 
+   4     6  1    2
+ 
+
+Input:
+
+The task is to complete the method which takes one argument, root of Binary Tree. There are multiple test cases. For each test case, this method will be called individually.
+
+Output:
+The function should return true if passed tree is Sum Tree, else false
+
+Constraints:
+1 <=T<= 30
+1 <=Number of nodes<= 1000
+1 <=Data of a node<= 1000
+
+Example:
+Input:
+2
+2
+3 1 L 3 2 R
+4
+10 20 L 10 30 R 20 40 L 20 60 R
+
+Output:
+1
+0
+
+--code--
+
+int in(Node *x,int* res)
+{
+    if(*res==1)
+    {
+    if(x==NULL)
+        return 0;
+    if(x->left!=NULL || x->right!=NULL)
+    {
+        int ls,rs;
+        ls=in(x->left,res);
+        rs=in(x->right,res);
+        if(x->data!=ls+rs)
+            *res=0;
+        return x->data+ls+rs;
+    }
+    return x->data;
+    }
+}
+bool isSumTree(Node* root)
+{
+    int res=1,ans;
+    ans=in(root,&res);
+    if(res==1)
+        return true;
+    else return false;
+}
